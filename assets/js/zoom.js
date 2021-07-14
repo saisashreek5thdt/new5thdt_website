c64f09816f566d15cd23031258b4ebacc226508d
saisashreek5thdt <sai@5thdt.com>
2021-07-14 16:54:58 +0530

    New assets and pages layouts were added

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,43 +1,43 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 	//change this value if you want to change the speed of the scale effect
-	var	scaleSpeed = 0.3,
-	//change this value if you want to set a different initial opacity for the .cd-half-block
+	var scaleSpeed = 0.3,
+		//change this value if you want to set a different initial opacity for the .cd-half-block
 		boxShadowOpacityInitialValue = 0.4,
-		animating = false; 
-	
+		animating = false;
+
 	//check the media query 
 	var MQ = window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
-	$(window).on('resize', function(){
+	$(window).on('resize', function () {
 		MQ = window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
 	});
 
 	//bind the animation to the window scroll event
 	triggerAnimation();
-	$(window).on('scroll', function(){
+	$(window).on('scroll', function () {
 		triggerAnimation();
 	});
 
 	//move to next/previous section
-    $('.cd-vertical-nav .cd-prev').on('click', function(){
-    	prevSection();
-    });
-    $('.cd-vertical-nav .cd-next').on('click', function(){
-    	nextSection();
-    });
-    $(document).keydown(function(event){
-		if( event.which=='38' ) {
+	$('.cd-vertical-nav .cd-prev').on('click', function () {
+		prevSection();
+	});
+	$('.cd-vertical-nav .cd-next').on('click', function () {
+		nextSection();
+	});
+	$(document).keydown(function (event) {
+		if (event.which == '38') {
 			prevSection();
 			event.preventDefault();
-		} else if( event.which=='40' ) {
+		} else if (event.which == '40') {
 			nextSection();
 			event.preventDefault();
 		}
 	});
 
-	function triggerAnimation(){
-		if(MQ == 'desktop') {
+	function triggerAnimation() {
+		if (MQ == 'desktop') {
 			//if on desktop screen - animate sections
-			(!window.requestAnimationFrame) ? animateSection() : window.requestAnimationFrame(animateSection);
+			(!window.requestAnimationFrame) ? animateSection(): window.requestAnimationFrame(animateSection);
 		} else {
 			//on mobile - remove the style added by jQuery 
 			$('.cd-section').find('.cd-block').removeAttr('style').find('.cd-half-block').removeAttr('style');
@@ -45,82 +45,82 @@ jQuery(document).ready(function($){
 		//update navigation arrows visibility
 		checkNavigation();
 	}
-	
-	function animateSection () {
+
+	function animateSection() {
 		var scrollTop = $(window).scrollTop(),
 			windowHeight = $(window).height(),
 			windowWidth = $(window).width();
-		
-		$('.cd-section').each(function(){
+
+		$('.cd-section').each(function () {
 			var actualBlock = $(this),
 				offset = scrollTop - actualBlock.offset().top,
 				scale = 1,
-				translate = windowWidth/2+'px',
+				translate = windowWidth / 2 + 'px',
 				opacity,
 				boxShadowOpacity;
 
-			if( offset >= -windowHeight && offset <= 0 ) {
+			if (offset >= -windowHeight && offset <= 0) {
 				//move the two .cd-half-block toward the center - no scale/opacity effect
 				scale = 1,
-				opacity = 1,
-				translate = (windowWidth * 0.5 * (- offset/windowHeight)).toFixed(0)+'px';
+					opacity = 1,
+					translate = (windowWidth * 0.5 * (-offset / windowHeight)).toFixed(0) + 'px';
 
-			} else if( offset > 0 && offset <= windowHeight ) {
+			} else if (offset > 0 && offset <= windowHeight) {
 				//the two .cd-half-block are in the center - scale the .cd-block element and reduce the opacity
-				translate = 0+'px',
-				scale = (1 - ( offset * scaleSpeed/windowHeight)).toFixed(5),
-				opacity = ( 1 - ( offset/windowHeight) ).toFixed(5);
+				translate = 0 + 'px',
+					scale = (1 - (offset * scaleSpeed / windowHeight)).toFixed(5),
+					opacity = (1 - (offset / windowHeight)).toFixed(5);
 
-			} else if( offset < -windowHeight ) {
+			} else if (offset < -windowHeight) {
 				//section not yet visible
 				scale = 1,
-				translate = windowWidth/2+'px',
-				opacity = 1;
+					translate = windowWidth / 2 + 'px',
+					opacity = 1;
 
 			} else {
 				//section not visible anymore
 				opacity = 0;
 			}
-			
-			boxShadowOpacity = parseInt(translate.replace('px', ''))*boxShadowOpacityInitialValue/20;
-			
+
+			boxShadowOpacity = parseInt(translate.replace('px', '')) * boxShadowOpacityInitialValue / 20;
+
 			//translate/scale section blocks
 			scaleBlock(actualBlock.find('.cd-block'), scale, opacity);
 
-			var directionFirstChild = ( actualBlock.is(':nth-of-type(even)') ) ? '-': '+';
-			var directionSecondChild = ( actualBlock.is(':nth-of-type(even)') ) ? '+': '-';
-			if(actualBlock.find('.cd-half-block')) {
-				translateBlock(actualBlock.find('.cd-half-block').eq(0), directionFirstChild+translate, boxShadowOpacity);
-				translateBlock(actualBlock.find('.cd-half-block').eq(1), directionSecondChild+translate, boxShadowOpacity);	
+			var directionFirstChild = (actualBlock.is(':nth-of-type(even)')) ? '-' : '+';
+			var directionSecondChild = (actualBlock.is(':nth-of-type(even)')) ? '+' : '-';
+			if (actualBlock.find('.cd-half-block')) {
+				translateBlock(actualBlock.find('.cd-half-block').eq(0), directionFirstChild + translate, boxShadowOpacity);
+				translateBlock(actualBlock.find('.cd-half-block').eq(1), directionSecondChild + translate, boxShadowOpacity);
 			}
 			//this is used to navigate through the sections
-			( offset >= 0 && offset < windowHeight ) ? actualBlock.addClass('is-visible') : actualBlock.removeClass('is-visible');		
+			(offset >= 0 && offset < windowHeight) ? actualBlock.addClass('is-visible'): actualBlock.removeClass('is-visible');
 		});
 	}
 
 	function translateBlock(elem, value, shadow) {
 		var position = Math.ceil(Math.abs(value.replace('px', '')));
-		
-		if( position >= $(window).width()/2 ) {
-			shadow = 0;	
-		} else if ( position > 20 ) {
+
+		if (position >= $(window).width() / 2) {
+			shadow = 0;
+		} else if (position > 20) {
 			shadow = boxShadowOpacityInitialValue;
 		}
 
 		elem.css({
-		    '-moz-transform': 'translateX(' + value + ')',
-		    '-webkit-transform': 'translateX(' + value + ')',
+			'-moz-transform': 'translateX(' + value + ')',
+			'-webkit-transform': 'translateX(' + value + ')',
 			'-ms-transform': 'translateX(' + value + ')',
 			'-o-transform': 'translateX(' + value + ')',
 			'transform': 'translateX(' + value + ')',
-			'box-shadow' : '0px 0px 40px rgba(0,0,0,'+shadow+')'
+			'box-shadow': '0px 0px 40px rgba(0,0,0,' + shadow + ')'
 		});
 	}
 
 	function scaleBlock(elem, value, opac) {
 		elem.css({
-		    '-moz-transform': 'scale(' + value + ')',
-		    '-webkit-transform': 'scale(' + value + ')',
+			'-moz-transform': 'scale(' + value + ')',
+			'-webkit-transform': 'scale(' + value + ')',
 			'-ms-transform': 'scale(' + value + ')',
 			'-o-transform': 'scale(' + value + ')',
 			'transform': 'scale(' + value + ')',
@@ -137,21 +137,25 @@ jQuery(document).ready(function($){
 	function prevSection() {
 		if (!animating) {
 			var prevSection = $('.cd-section.is-visible');
-			if(prevSection.length > 0 && $(window).scrollTop() != prevSection.offset().top) {
+			if (prevSection.length > 0 && $(window).scrollTop() != prevSection.offset().top) {
 				smoothScroll(prevSection);
-			} else if(prevSection.prev().length > 0 && $(window).scrollTop() == prevSection.offset().top) {
+			} else if (prevSection.prev().length > 0 && $(window).scrollTop() == prevSection.offset().top) {
 				smoothScroll(prevSection.prev('.cd-section'));
 			}
 		}
 	}
 
 	function checkNavigation() {
-		( $(window).scrollTop() < $(window).height()/2 ) ? $('.cd-vertical-nav .cd-prev').addClass('inactive') : $('.cd-vertical-nav .cd-prev').removeClass('inactive');
-		( $(window).scrollTop() > $(document).height() - 3*$(window).height()/2 ) ? $('.cd-vertical-nav .cd-next').addClass('inactive') : $('.cd-vertical-nav .cd-next').removeClass('inactive');
+		($(window).scrollTop() < $(window).height() / 2) ? $('.cd-vertical-nav .cd-prev').addClass('inactive'): $('.cd-vertical-nav .cd-prev').removeClass('inactive');
+		($(window).scrollTop() > $(document).height() - 3 * $(window).height() / 2) ? $('.cd-vertical-nav .cd-next').addClass('inactive'): $('.cd-vertical-nav .cd-next').removeClass('inactive');
 	}
 
 	function smoothScroll(target) {
 		animating = true;
-        $('body,html').animate({'scrollTop': target.offset().top}, 500, function(){ animating = false; });
+		$('body,html').animate({
+			'scrollTop': target.offset().top
+		}, 500, function () {
+			animating = false;
+		});
 	}
 });
